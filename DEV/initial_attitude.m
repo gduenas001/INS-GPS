b0922c0b2885cf4e67e34fabcbbcc3d797fa7a11
gduenas001 <gdueasar@hawk.iit.edu>
2018-04-05 18:49:31 -0500

    code ready to push

---
+++ DEV/initial_attitude.m
@@ -0,0 +1,24 @@
+
+function [phi,theta]= initial_attitude(iu)
+
+mu= mean(iu,2);
+
+theta= atan2( mu(1) , abs(mu(3)) ); % for z-axis pointing down (accz < 0)
+phi= -atan2( mu(2) , abs(mu(3)) );   % for z-axis pointing down (accz < 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
