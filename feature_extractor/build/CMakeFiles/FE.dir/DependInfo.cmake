2906c437905cba9832e38ad4a486723a67b22b58
Osama.Robotics-Lab <oabdulhafez@hawk.iit.edu>
2018-12-12 13:32:52 -0600

    add feature extractor build file and remove draft FE output

---
+++ feature_extractor/build/CMakeFiles/FE.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/osamarobotics-lab/OH-FE_copy/src/FE.cpp" "/home/osamarobotics-lab/OH-FE_copy/build/CMakeFiles/FE.dir/src/FE.cpp.o"
+  "/home/osamarobotics-lab/OH-FE_copy/src/compute_cloud_parameters.cpp" "/home/osamarobotics-lab/OH-FE_copy/build/CMakeFiles/FE.dir/src/compute_cloud_parameters.cpp.o"
+  "/home/osamarobotics-lab/OH-FE_copy/src/configure_viewer.cpp" "/home/osamarobotics-lab/OH-FE_copy/build/CMakeFiles/FE.dir/src/configure_viewer.cpp.o"
+  "/home/osamarobotics-lab/OH-FE_copy/src/create_clusters.cpp" "/home/osamarobotics-lab/OH-FE_copy/build/CMakeFiles/FE.dir/src/create_clusters.cpp.o"
+  "/home/osamarobotics-lab/OH-FE_copy/src/read_inputs.cpp" "/home/osamarobotics-lab/OH-FE_copy/build/CMakeFiles/FE.dir/src/read_inputs.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_DAVIDSDK"
+  "DISABLE_DSSDK"
+  "DISABLE_ENSENSO"
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI2"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "DISABLE_RSSDK"
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  "qh_QHpointer"
+  "vtkDomainsChemistry_AUTOINIT=1(vtkDomainsChemistryOpenGL2)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  "vtkRenderingOpenGL2_AUTOINIT=1(vtkRenderingGL2PSOpenGL2)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/vtk-7.1"
+  "/usr/local/include/pcl-1.8"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "../src/h_files"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ feature_extractor/build/CMakeFiles/FE.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FE.dir/src/FE.cpp.o"
+  "CMakeFiles/FE.dir/src/compute_cloud_parameters.cpp.o"
+  "CMakeFiles/FE.dir/src/configure_viewer.cpp.o"
+  "CMakeFiles/FE.dir/src/create_clusters.cpp.o"
+  "CMakeFiles/FE.dir/src/read_inputs.cpp.o"
+  "FE.pdb"
+  "FE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
